b4a4b085dde6f51fac1d650d9dc2443f225e85fc
Ketan Sonar <ketan.sonar.798@gmail.com>
2023-09-29 14:02:59 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Synonyms
+
+A simple web app that shows the synonyms of a word.
+
+## Tech Stack
+
+1. NextJS 13
+1. TypeScript
+1. axios for doing API calls
+
+## Local Setup
+
+```sh
+npm install
+npm run dev
+```
---
+++ src/app/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import axios from "axios";
+import { RefObject, useEffect, useRef, useState } from "react";
+
+const API_URL = "https://api.api-ninjas.com/v1/thesaurus?word=";
+
+export default function Home() {
+  const [word, setWord] = useState("");
+  const [synonyms, setSynonyms] = useState<string[]>([]);
+  const inputRef = useRef() as RefObject<HTMLInputElement>;
+
+  useEffect(() => {
+    if (word) getSynonyms();
+    inputRef.current?.focus();
+  }, []);
+
+  const getSynonyms = async () => {
+    const res = await axios.get(API_URL + word, {
+      headers: { "X-Api-Key": "gyyejB4K8GtHf0ukzl4pHA==Hw766ayiyKPPmn1Z" },
+    });
+    setSynonyms(res.data.synonyms);
+  };
+
+  const handleClickWord = (w: string) => {
+    setWord(w);
+    getSynonyms();
+  };
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="container max-w-md flex flex-col rounded border p-6">
+        <h1 className="text-3xl text-center">Synonyms</h1>
+        <hr className="w-full" />
+        <div className="WordInput my-2 flex">
+          <input
+            className="px-2 py-1 grow text-xl border border-black rounded-l focus:outline-none"
+            type="text"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && getSynonyms()}
+            ref={inputRef}
+          />
+          <button
+            className="bg-black text-white px-2 py-1 text-xl rounded-r"
+            onClick={getSynonyms}
+          >
+            Search
+          </button>
+        </div>
+        <div className="SynonymsList text-xl flex justify-center p-4">
+          <ul className="space-y-2">
+            {synonyms.slice(0, 5).map((s, i) => (
+              <li key={i}>
+                <button
+                  className="underline"
+                  onClick={() => handleClickWord(s)}
+                >
+                  {`${i + 1}. ${s}`}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
